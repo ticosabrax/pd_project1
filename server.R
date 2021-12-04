e9345990c592844af34777d4746c18716ac19de5
Edgar Sabán <esaban@MacBook-Pro-de-Edgar.local>
2021-12-04 13:31:22 -0600

    Dashboard Base.

---
+++ server.R
@@ -0,0 +1,18 @@
+shinyServer(function(input, output, session) {
+  
+  
+  # Same as above, but with fill=TRUE
+  output$progressBox2 <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple", fill = TRUE
+    )
+  })
+  output$approvalBox2 <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow", fill = TRUE
+    )
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,82 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(title = "UFO Insighting"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("UFO Dashboard", tabName = "dashboard", icon = icon("reddit-alien")),
+      menuItem("UFO Sighting by Date", tabName = "ufo_date", icon = icon("calendar-alt")),
+      menuItem("UFO Sighting by Time", tabName = "ufo_time", icon = icon("stopwatch")),
+      menuItem("UFO Sighting City Mapped", tabName = "ufo_map", icon = icon("globe-americas")),
+      menuItem("UFO Sighting Basic", tabName = "ufo_basic", icon = icon("chart-area"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(
+                  width = 12,
+                  h1("UFO Sighting"),
+                  "The National UFO Research Center (NUFORC) collects and serves over 100,000 reports of UFO sightings.
+                  This dataset contains the report content itself including the time, location duration, and other attributes in both the raw form as it is recorded on the NUFORC site as well as a refined, standardized form that also contains lat/lon coordinates."
+                )
+              ),
+              fluidRow(
+                infoBox("UFO Sightings", 10 * 2, icon = icon("reddit-alien"), fill = TRUE, color = "green"),
+                infoBox("UFO Sighting Shapes", 10 * 2, icon = icon("vector-square"), fill = TRUE),
+                infoBox("UFO Sighting Cities", 10 * 2, icon = icon("city"), fill = TRUE, color = "yellow")
+              ),
+              fluidRow(
+                
+              )
+      ),
+      tabItem(tabName = "ufo_date",
+              fluidRow(
+                box(status = "primary", 
+                    title = "Filtros",
+                    solidHeader = TRUE,
+                    
+                    dateRangeInput("ufo_date_range", "Rango de fechas"),
+                    radioButtons("ufo_date_period", label ="Período", choices = list("Año" = 1,
+                                                                              "Mes" = 2,
+                                                                              "Día" = 3)),
+                    helpText("Nota: Se mostrará una gráfica con los conteos de UFO Sightings
+                             (abistamiento de OVNIS) en el rango de fechas seleccionado agrupado por el período."),
+                    actionButton("ufo_date_btn", "Ver UFO Sightings", icon = icon("space-shuttle"))
+                ),
+                box(
+                  title = "Output",
+                )
+              )
+      ),
+      tabItem(tabName = "ufo_time",
+              
+      ),
+      tabItem(tabName = "ufo_map",
+              fluidRow(
+                box(status = "primary", 
+                    title = "Filtros",
+                    solidHeader = TRUE,
+                    
+                    selectInput("ufo_map_shape", "Shape", choices = list("Cube" = 1,
+                                                                                     "Circle" = 2,
+                                                                                     "Light" = 3)),
+                    helpText("Nota: Se mostrará una mapa de los USA indicando los conteos de UFO Sightings
+                             (abistamiento de OVNIS) que han sucedido en los estados correspondientes."),
+                    actionButton("ufo_date_btn", "Ver UFO Sightings", icon = icon("space-shuttle"))
+                ),
+                box(
+                  title = "Output",
+                )
+              )
+              
+      ),
+      tabItem(tabName = "ufo_basic",
+              
+      )
+    )
+  )
+)
