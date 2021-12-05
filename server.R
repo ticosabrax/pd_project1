267e6038f60aa4d44283c9ab51cf42336b1c2d32
Edgar Sabán <esaban@MacBook-Pro-de-Edgar.local>
2021-12-05 03:31:46 -0600

    Incluye: 1. Upload file 2. UFO Sighting by Date 3. UFO Sighting State Mapped

--- server.R
+++ server.R
@@ -1,18 +1,192 @@
+library(DT)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(leaflet)
+library(shinyalert)
+
+options(shiny.maxRequestSize = 30*1024^2)
+
+
+
 shinyServer(function(input, output, session) {
   
+  fileUpload <- reactive({
+    content_file <- input$ufo_file
+    if(is.null(content_file)){
+      return(NULL)
+    }
+    return(read.csv(content_file$datapath))
+    
+  })
+
+  # ------------ UFO Dashboard --------------------
+  # Renderizando la table del dataset
+  output$ufo_dataset <- DT::renderDataTable({
+      dt <- fileUpload()
+      dt[, input$show_vars] %>%
+      DT::datatable(extensions = 'Buttons',
+                    options = list(buttons = c("csv", "pdf"),
+                                   dom = 'lfrtipB'),
+                    filter = list(position = 'top'),
+                    rownames = FALSE)
+      return(dt)
+  })
   
-  # Same as above, but with fill=TRUE
-  output$progressBox2 <- renderInfoBox({
-    infoBox(
-      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
-      color = "purple", fill = TRUE
-    )
+  # Renderizando las estadísticas generales
+  
+  output$ufo_sightings <- renderInfoBox({
+    dt <- fileUpload()
+    infoBox("UFO Sightings", format(nrow(dt), big.mark = ","),"Total", icon = icon("reddit-alien"), fill = TRUE, color = "green")
   })
-  output$approvalBox2 <- renderInfoBox({
-    infoBox(
-      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
-      color = "yellow", fill = TRUE
-    )
+  
+  output$ufo_sightings_shapes <- renderInfoBox({
+    dt <- fileUpload()
+    if (!is.null(dt)){
+      infoBox("UFO Sighting Shapes", nrow(dt %>% distinct(shape)), "Total", icon = icon("vector-square"), fill = TRUE)
+    }else{
+      infoBox("UFO Sighting Shapes", "NULL", "Total", icon = icon("vector-square"), fill = TRUE)
+    }
+    
   })
+  
+  output$ufo_sightings_cities <- renderInfoBox({
+    dt <- fileUpload()
+    if (!is.null(dt)){
+      infoBox("UFO Sighting Cities", format(nrow(fileUpload() %>% distinct(city)), big.mark = ","), "Around all USA", icon = icon("city"), fill = TRUE, color = "yellow")
+    }else{
+      infoBox("UFO Sighting Cities", "NULL", "Around all USA", icon = icon("city"), fill = TRUE, color = "yellow")
+    }
+    
+  })
+  
+  # ------------ UFO Sighting by Date --------------------
+  
+  # Reactive para actualizar dataset según el ufo_date_range
+  getDTDate <- reactive({
+    dt <- fileUpload()
+    if (!is.null(dt)){
+      if (input$ufo_date_period == "year"){
+        result <- dt %>%
+          mutate(date = as.Date(date_time), year = year(date)) %>%
+          filter(between(date, as.Date(input$ufo_date_range[1]),as.Date(input$ufo_date_range[2]))) %>%
+          group_by(year) %>%
+          summarise(n = n())  
+      }else if (input$ufo_date_period == "month"){
+        result <- dt %>%
+          mutate(date = as.Date(date_time), month = month(date)) %>%
+          filter(between(date, as.Date(input$ufo_date_range[1]),as.Date(input$ufo_date_range[2]))) %>%
+          group_by(month) %>%
+          summarise(n = n())
+      }else if (input$ufo_date_period == "day"){
+        result <- dt %>%
+          mutate(date = as.Date(date_time), day = day(date)) %>%
+          filter(between(date, as.Date(input$ufo_date_range[1]),as.Date(input$ufo_date_range[2]))) %>%
+          group_by(day) %>%
+          summarise(n = n())
+      }
+      return(result)
+    }
+
+  })
+  
+  # Renderizando y creando el plot
+  output$ufo_date_plot <- renderPlot({
+    dt <- getDTDate()
+    
+    if (!is.null(dt)){
+      if (input$ufo_date_period == "year"){
+        p <- ggplot(dt, aes(factor(year), n)) +
+          geom_segment( aes(x=factor(year), xend=factor(year), y=0, yend=n), color="grey")
+      }else if (input$ufo_date_period == "month"){
+        p <- ggplot(dt, aes(factor(month), n)) +
+          geom_segment( aes(x=factor(month), xend=factor(month), y=0, yend=n), color="grey")
+      }else if (input$ufo_date_period == "day"){
+        p <- ggplot(dt, aes(factor(day), n)) +
+          geom_segment( aes(x=factor(day), xend=factor(day), y=0, yend=n), color="grey")
+      }
+      
+      p<- p +
+        geom_point(shape = 21, colour = input$ufo_date_color, fill = "white", size = 2, stroke = 1) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+        xlab(input$ufo_date_period) + ylab("Total")
+      
+      return(p)
+    }
+    
+  })
+  
+  # Renderizando la table del dataset
+   output$ufo_date_dataset <- DT::renderDataTable({
+     dt <- getDTDate()
+     if (!is.null(dt)){
+       dt%>%
+         DT::datatable(extensions = 'Buttons',
+                       options = list(buttons = c("csv", "pdf"),
+                                      dom = 'lfrtipB'),
+                       filter = list(position = 'top'),
+                       rownames = NULL,
+         ) %>%
+         formatRound(c("n"), mark = ",",digits = 0)
+     }
+     
+   })
+   
+   # ------------ UFO Sighting by State mapped --------------------
+   
+   # Reactive para actualizar dataset según el ufo_map_state y ufo_map_shape
+   getMap <- reactive({
+     dt <- fileUpload()
+     if (!is.null(dt)){
+       name_shapes <- dt$shape %>% unique()
+       map <- dt %>%
+         filter(state == input$ufo_map_state)
+       
+       if (input$ufo_map_shape != ""){
+         if (input$ufo_map_shape %in% name_shapes){
+           map <- map %>%
+             filter(shape == input$ufo_map_shape)
+         }
+       }
+       
+       unique(map[,c("city_latitude","city_longitude")])
+       return(map)
+     }
+     
+   })
+   
+   # Renderizando el mapa
+   output$ufo_map_draw <- renderLeaflet({
+     
+     dt <- fileUpload()
+     if (!is.null(dt)){
+       name_shapes <- dt$shape %>% unique()
+       
+       color_shapes <- c('#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2')
+       pal <- colorFactor(color_shapes, domain = name_shapes)
+       
+       m <- leaflet() %>% addTiles() %>%
+         addCircles(data = getMap(), lat = ~city_latitude, lng = ~city_longitude,
+                    color = ~pal(shape), label = ~shape, opacity = 1)
+       
+       m <- m %>% addLegend(data = getMap(), "bottomright", pal = pal,
+                            values = ~shape, title = "Shapes",
+                            opacity = 0.7)
+       
+       return(m)
+     }
+   })
+   
+   # Renderizando la table del dataset
+   output$ufo_map_dataset <- DT::renderDataTable({
+     getMap() %>%
+       DT::datatable(extensions = 'Buttons',
+                     options = list(buttons = c("csv", "pdf"),
+                                    dom = 'lfrtipB'),
+                     filter = list(position = 'top'),
+                     rownames = NULL,
+       )
+   })
   
 })
